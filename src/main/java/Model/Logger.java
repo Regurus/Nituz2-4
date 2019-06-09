f2d6ebfe50f2f76122835b9452f3ec7d7131237e
Inbaroth <inbaroth1993@gmail.com>
2019-06-09 18:17:38 +0300

    18:17

--- src/main/java/Model/Logger.java
+++ src/main/java/Model/Logger.java
@@ -1,18 +1,6 @@
-//package Model;
-//
-//
-//
-//public abstract class Logger {
-//
-//    private static Logger logger = null;
-//
-//
-//    private Logger(){
-//     
-//    }
-//
-//    public static Logger UsersControllerInstance(UsersDatabase usersDB ,ComplaintDatabase complaintDB) {
-//        return null;
-//    }
-//
-//}
+package Model;
+
+public interface Logger {
+
+
+}
