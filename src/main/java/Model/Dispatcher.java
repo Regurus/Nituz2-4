e70a8961660730c5b943c83eb4f2a612465866fc
Ariel <arielkam@post.bgu.ac.il>
2019-06-09 17:08:10 +0300

    17:07

--- src/main/java/Model/Dispatcher.java
+++ src/main/java/Model/Dispatcher.java
@@ -6,4 +6,7 @@ public class Dispatcher extends User {
     public Dispatcher(String userName, String password, String name,String rank, String status,String type, UsersController uc){
         super(userName,password,name,rank,status,"Dispature", uc);
     }
+
+
+
 }
