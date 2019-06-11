23cb3d73439a225c7a957a39a44485b3adeaac87
Ariel <arielkam@post.bgu.ac.il>
2019-06-11 20:11:42 +0300

    Admin userDB

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -31,6 +31,10 @@ public class Main extends Application {
     }
     public static void main(String[] args) {
         launch(args);
+
+
+
+
         /*ComplaintDatabase cd = new ComplaintDatabase();
         System.out.println(cd.getLastIndex());
 
